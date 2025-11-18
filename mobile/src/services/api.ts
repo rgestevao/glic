66000203574ef056857ea46713c9452e0cefeba7
rodrigoges <rgestevao@gmail.com>
2025-11-18 16:24:23 -0300

    feat: add icon and splashscreen

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import axios from 'axios'
 
-const API_URL = 'http://192.168.1.7:8080/api/glic';
+const API_URL = 'https://glic-api.onrender.com/api/glic';
 
 export const api = axios.create({
   baseURL: API_URL,
